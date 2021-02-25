c8b7be8da0acfa6f846bebb7b291a27f9d83a9b5
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-02-24 20:49:50 -0800

    Sign Up form complete and functional

---
+++ anywhere-fitness/src/components/Forms/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Form = styled.form `
+
+`;
+export const Container = styled.div `
+
+`;
+
+export const Label = styled.label `
+
+`;
+
+export const Input = styled.input `
+
+`;
