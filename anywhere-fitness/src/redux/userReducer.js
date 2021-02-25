acfec379a192527c3b738410386f0fff25033f9e
Kyle Willard <kyleswillard@gmail.com>
2021-02-24 16:50:49 -0800

    userReducer started, initial state added, notes added.

---
+++ anywhere-fitness/src/redux/userReducer.js
@@ -0,0 +1,41 @@
+import { CREATE_CLASS } from '../actions/types'
+import {
+    CREATE_USER,
+    USER_FAIL,
+    LOGIN_USER,
+    LOGOUT_USER,
+    LOAD_USER
+} from '../actions/userActions'
+
+//isAuth === is authenticated by server
+const initialState = {
+username: '',
+email: '',
+password: '',
+token: '',
+isAuth: false,
+isLoading: false
+error: null
+}
+
+const userReducer = (state = initialState, actions) => {
+Switch(actions.type) {
+  case CREATE_USER:
+    //will set token & !load, !auth
+    return state;
+  case: USER_FAIL:
+  // will set err & clear load
+  return state;
+  case LOGIN_USER:
+    // will !load, !auth,
+    return state;
+  case LOGOUT_USER:
+    // Will revert to initialState
+    return state
+  case LOAD_USER:
+    // Will isLoading: true
+    return state
+  default:
+    return state;
+}
+}
