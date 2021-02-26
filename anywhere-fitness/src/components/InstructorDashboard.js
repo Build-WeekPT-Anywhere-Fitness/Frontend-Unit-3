3579b58ed2d419726c2029d014e88892f5dc174e
kato hawkins <kristhahawkins@icloud.com>
2021-02-25 21:02:32 -0500

    attempting to pull from main

--- anywhere-fitness/src/components/InstructorDashboard.js
+++ anywhere-fitness/src/components/InstructorDashboard.js
@@ -1,7 +1,7 @@
 import React,{useEffect} from "react";
 import {fetchClasses} from "../actions/classActions"
 import ClassCard from "./ClassCard"
-
+import CreateClass from "./CreateClass"
 const InstructorDashboard = () =>{
 
         useEffect(() =>{
