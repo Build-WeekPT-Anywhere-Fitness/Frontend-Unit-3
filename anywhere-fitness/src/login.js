4e6936bb125435340c722019ed93ecbd8145bbae
ACarrill42 <austincarrill@gmail.com>
2021-03-02 14:21:58 -0600

    working on post request

--- anywhere-fitness/src/login.js
+++ anywhere-fitness/src/login.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import FormSchema from './validation';
 import Axios from 'axios';
 import {Form, Label, Input} from './styling'
+import { ValidationError } from 'yup';
+import axios from 'axios';
 function StateInfo() {
   username: ''
   password: ''
@@ -24,10 +26,47 @@ function Login() {
   useEffect(() => {
     FormSchema.isValid(infoState)
       .then(valid => {
-
+        setDisabledButton(!valid);
       })
-  })
-  
+  }, [infoState])
+
+  const inputChange = e => {
+    e.persist();
+    const newData = {
+      ...infoState, 
+      [e.target.name]:e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    }
+    validation(e);
+    setInfoState(newData);
+  }
+
+  const validation = e => {
+    yup
+      .reach(FormSchema, e.target.name)
+      .validate(e.target.value)
+      .then(valid => {
+        setErrors({...errors, [e.target.name]: ''});
+      })
+      .catch(error => {
+        setErrors({...errors, [e.target.name]: error.errors[0]});
+      })
+  }
+
+  const formSubmit = e => {
+    e.preventDefault();
+    axios.post("https://anytime-fitness.herokuapp.com/api/auth/login", infoState)
+    .then(response => {
+      setUser(response.data);
+      setInfoState({
+        username: '',
+        password: '',
+      })
+    })
+    .catch(error => {
+      console.log(error.response)
+    })
+  }
+
   return (
     <Form className = 'login'>
       <Label> Username: 
