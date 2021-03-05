68c53f53bfa7d9d984f1fcd2d0507a8625421213
ACarrill42 <austincarrill@gmail.com>
2021-03-04 19:59:48 -0600

    added import to App.js for Login

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import SignUpForm from './components/Forms/SignUp';
+import Login from './login'
 function App() {
   return (
     <div className="App">
       <SignUpForm />
+      <Login />
     </div>
   );
 }
